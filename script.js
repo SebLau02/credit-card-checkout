a15d110a6a00d40a0df42a3c7dfd20bea1219ad8
sebastien lau <sebastien.lau2021@gmail.com>
2023-09-02 16:48:15 +0200

    responsive design and js func refactored

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const frontFace = document.querySelector(".card-front-face");
 const backFace = document.querySelector(".card-back-face");
 
+const inputCardNumber = document.querySelectorAll(".input-card-number");
+const inputExpirationDate = document.querySelectorAll(".input-expiration-date");
+const inputCardOwner = document.querySelector(".card-owner");
+const inputCardCrypto = document.querySelector(".card-crypto");
+
+const cardNumber = document.querySelectorAll(".card-number");
+const expirationDate = document.querySelectorAll(".date");
+const cardOwner = document.querySelector(".owner");
+const cardCrypto = document.querySelector(".cryptogram");
+
+const regexNumber = /^\d+$/;
+
+//********** Foregrounded card faces when clicked **********
+
 frontFace.addEventListener("click", () => {
 	frontFace.classList.remove("z-0");
 	backFace.classList.remove("z-10");
@@ -15,53 +29,63 @@ const backFaceForeground = () => {
 
 backFace.addEventListener("click", backFaceForeground);
 
-const inputCardNumber = document.querySelectorAll(".input-card-number");
-const inputExpirationDate = document.querySelectorAll(".input-expiration-date");
-const inputCardOwner = document.querySelector("#card-owner");
-const inputCardCrypto = document.querySelector("#card-crypto");
+//********** update dom element with form values function **********
 
-const cardNumber = document.querySelectorAll(".card-number");
-const expirationDate = document.querySelectorAll(".date");
-const cardOwner = document.querySelector(".owner");
-const cardCrypto = document.querySelector(".cryptogram");
+const handleInputAndUpdateElement = (
+	e,
+	i,
+	nodeList,
+	element,
+	elementLength,
+) => {
+	if (regexNumber.test(e.target.value)) {
+		//********** update dom element **********
 
-const regexNumber = /^\d+$/;
+		element[i].innerHTML = e.target.value;
+
+		//********** focus on next input when input maxlength is reached **********
+
+		if (element[i].textContent.length === elementLength) {
+			nodeList[i].nextElementSibling.focus();
+		}
+	}
+};
+
+//********** card number listener **********
 
 for (let i = 0; i < inputCardNumber.length; i++) {
 	inputCardNumber[i].addEventListener("keyup", (e) => {
-		if (regexNumber.test(e.target.value)) {
-			cardNumber[i].innerHTML = e.target.value;
-
-			if (cardNumber[i].textContent.length === 4) {
-				inputCardNumber[i].nextElementSibling.focus();
-			}
-		}
+		handleInputAndUpdateElement(e, i, inputCardNumber, cardNumber, 4);
 	});
 }
+
+//********** expiration date listerner **********
 
 for (let i = 0; i < inputExpirationDate.length; i++) {
 	inputExpirationDate[i].addEventListener("keyup", (e) => {
-		if (regexNumber.test(e.target.value)) {
-			expirationDate[i].innerHTML = e.target.value;
-
-			if (expirationDate[i].textContent.length === 2) {
-				inputExpirationDate[i].nextElementSibling.focus();
-			}
-		}
+		handleInputAndUpdateElement(
+			e,
+			i,
+			inputExpirationDate,
+			expirationDate,
+			2,
+		);
 	});
 }
 
-const inputContent = (element, e) => {
+//********** owner and card crypto update func **********
+
+const elementContentUpdate = (element, e) => {
 	element.innerHTML = e.target.value;
 };
 
 inputCardOwner.addEventListener("keyup", (e) => {
-	inputContent(cardOwner, e);
+	elementContentUpdate(cardOwner, e);
 });
 
 inputCardCrypto.addEventListener("keyup", (e) => {
 	if (regexNumber.test(e.target.value)) {
-		inputContent(cardCrypto, e);
+		elementContentUpdate(cardCrypto, e);
 		backFaceForeground();
 	}
 });
