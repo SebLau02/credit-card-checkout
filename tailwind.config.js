f556b14ce658ef1696cdfabe86dd35a106484324
sebastien lau <sebastien.lau2021@gmail.com>
2023-09-04 13:36:22 +0200

    neumorphism effect on cb

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const myUtilities = {
+        ".text-neo": {
+          "text-shadow":
+            " -0.5px 0.5px 2px #fbe05b, 0.4px -0.4px #2b2b2b, 1px -1px 3px #2b2b2b",
+        },
+      };
+      addUtilities(myUtilities);
+    }),
+  ],
 };
