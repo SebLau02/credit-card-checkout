4240a63c953ddefb1e051431df6a6841e7d04cb9
sebastien lau <sebastien.lau2021@gmail.com>
2023-09-04 17:59:11 +0200

    adjusment responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,29 @@ module.exports = {
   },
   plugins: [
     plugin(function ({ addUtilities }) {
-      const myUtilities = {
+      const textStyle = {
         ".text-neo": {
           "text-shadow":
             " -0.5px 0.5px 2px #fbe05b, 0.4px -0.4px #2b2b2b, 1px -1px 3px #2b2b2b",
         },
       };
-      addUtilities(myUtilities);
+
+      const marges = {
+        ".mr-20perc": {
+          "margin-left": "50%",
+        },
+      };
+      const position = {
+        ".top-20perc": {
+          top: "20%",
+        },
+        ".left-20perc": {
+          left: "20%",
+        },
+      };
+      addUtilities(textStyle);
+      addUtilities(marges);
+      addUtilities(position);
     }),
   ],
 };
